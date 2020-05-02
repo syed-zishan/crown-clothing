57b1dc54a860ac26d1a91a78081ac7406cd3553d
syed-zishan <64665442+syed-zishan@users.noreply.github.com>
2020-05-02 21:41:43 +0530

    CC-2/Story/AddHomePage (#2)
    * feat(CC-2): Add Home Page
    
    * feat(CC-2): Add Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+
+import { HomePage } from './homepage.component';
 import './App.css';
 
 function App() {
   return (
-    <div>App Root</div>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
