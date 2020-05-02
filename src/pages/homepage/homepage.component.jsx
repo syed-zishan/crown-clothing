b7ff090510464a4aa42616c258a08eed39773c84
syed-zishan <64665442+syed-zishan@users.noreply.github.com>
2020-05-02 23:56:05 +0530

    fix(CC-3): Separate Components From HomePage Components (#3)

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DirectoryMenu from '../../components/directory/directory-menu.component';
+
+import './homepage.styles.scss';
+
+export const HomePage = ()=> (
+    <div className='homepage'>
+       <DirectoryMenu />
+    </div>
+);
