7e52ce7a720a8430c011db431ee95e5e38008f54
syed-zishan <64665442+syed-zishan@users.noreply.github.com>
2020-05-03 20:23:03 +0530

    feat(CC-4): Added React-Router-Dom (#4)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
 import { HomePage } from './pages/homepage/homepage.component';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import './App.css';
+
+
+const HatsPage = ()=>(
+  <div> Hats Page </div>
+);
+
+const NotFoundPage = ()=>{
+  var location = useLocation();
+  return(
+    <div> 404 Page You Are Looking For "{location.pathname}" Is Not Found!! </div>
+  );
+}
+  
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact={true} path='/' component = {HomePage}/>
+        <Route exact={true} path='/shop/hats' component = {HatsPage}/>
+        <Route  path='*' component = {NotFoundPage}/>
+      </Switch>
+      
     </div>
   );
 }
