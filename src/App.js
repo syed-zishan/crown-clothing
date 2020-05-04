dcfbeb2099671715c1f8ea9a7d529f99823722e1
syed-zishan <64665442+syed-zishan@users.noreply.github.com>
2020-05-05 04:04:08 +0530

    feat(CC-6): Added Header Component (#6)

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import React from 'react';
 import { HomePage } from './pages/homepage/homepage.component';
 import { Route, Switch, useLocation } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
 import './App.css';
 
-const NotFoundPage = ()=>{
+const NotFoundPage = () => {
   var location = useLocation();
-  return(
-    <div> 404 Page You Are Looking For "{location.pathname}" Is Not Found!! </div>
+  return (
+    <div>404 Page You Are Looking For "{location.pathname}" Is Not Found!!</div>
   );
-}
-  
-
+};
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact={true} path='/' component = {HomePage}/>
-        <Route exact={true} path='/shop' component = {ShopPage}/>
-        <Route  path='*' component = {NotFoundPage}/>
+        <Route exact={true} path='/' component={HomePage} />
+        <Route exact={true} path='/shop' component={ShopPage} />
+        <Route path='*' component={NotFoundPage} />
       </Switch>
-      
     </div>
   );
 }
