9a05527279d8d7192bff3f2c27b8bdb83a24b8d0
syed-zishan <syedzishan0591@gmail.com>
2020-05-20 21:42:41 +0530

    feat(CC-6): Add Sign In Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HomePage } from './pages/homepage/homepage.component';
 import { Route, Switch, useLocation } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import './App.css';
 
@@ -20,6 +21,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
+        <Route exact={true} path='/signin' component={SignInAndSignUp} />
         <Route exact={true} path='/shop' component={ShopPage} />
         <Route path='*' component={NotFoundPage} />
       </Switch>
